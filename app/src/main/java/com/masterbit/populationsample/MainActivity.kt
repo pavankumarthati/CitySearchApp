292160dee29e96e5cc7c83bb204bf1a03360b795
Pavan kumar Thati <pavankumar.thati@citrix.com>
2022-01-22 16:24:45 +0530

    view binding.

--- app/src/main/java/com/masterbit/populationsample/MainActivity.kt
+++ app/src/main/java/com/masterbit/populationsample/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.masterbit.populationsample
 
 import android.os.Bundle
-import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.viewModels
@@ -13,6 +12,7 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.jakewharton.rxbinding3.widget.textChanges
+import com.masterbit.populationsample.databinding.ActivityMainBinding
 import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
@@ -25,27 +25,28 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val populationRv = findViewById<RecyclerView>(R.id.populationRv)
-        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
-        populationRv.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
-        populationRv.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        binding.populationRv.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        binding.populationRv.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
 
-        populationRv.adapter = PopulationItemAdapter()
+        binding.populationRv.adapter = PopulationItemAdapter()
 
         val tvObservable = findViewById<TextView>(R.id.tv).textChanges().debounce(600, TimeUnit.MILLISECONDS)
 
         viewModel.filterData(tvObservable.filter { it.isNotBlank() })
 
         viewModel.getCityPopulation().observe(this) {
-            (populationRv.adapter as PopulationItemAdapter).submitList(it.data)
+            (binding.populationRv.adapter as PopulationItemAdapter).submitList(it.data)
         }
 
         viewModel.getProgressBarLiveData().observe(this) {
-            progressBar.isVisible = it
+            binding.progressBar.isVisible = it
         }
 
         viewModel.onError().observe(this) {
